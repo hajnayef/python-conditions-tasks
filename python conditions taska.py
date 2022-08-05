d5dc381d207b7cb35e7cdbd0a7c79bc99dda0080
donia haj nayef <doniahajnayef@gmail.com>
2022-08-05 17:34:30 +0200

    python condionts tasks

---
+++ python conditions taska.py
@@ -0,0 +1,87 @@
+
+#1
+x = 10
+
+if x < 15:
+    print("f")
+
+#2
+    
+x = 10
+
+
+if x< 15:
+    print(" x is smaller than 15")
+    
+
+#3
+we use all, when there are more and
+
+#4
+If there are many (and),We can use all instead of.
+
+#5
+If there are many (or),We can use any instead of.
+
+#6
+
+
+#7
+we use (if ) for some code if one condition is true
+elif only if the preceding statement is false,
+
+#8
+
+elif and else only if the preceding statement is false.
+we can use elif more than once, but we use else only once.
+
+
+#9
+yes, we can use elif more one
+
+
+
+#10
+no, we can not use else more than one
+
+
+
+#11
+name= "donia"
+password= 12345
+
+if name==" donia" and password== 12345
+
+
+
+#12
+
+
+
+
+#13
+
+
+the elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
+
+
+
+#14
+
+numbers= [2,4,6,8,10]
+
+if 4 in numbers:
+
+
+
+if 4 and 5 in numbers:
+
+
+if 3 or 6 in numbers:
+
+
+if all([2,4,5]):
+
+
+
+
